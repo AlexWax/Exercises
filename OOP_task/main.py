eb948979f5e30dda965dc6b2020aae3befc2dbfd
AlexWax <leha.voskoboynik@mail.ru>
2025-05-16 08:55:36 +0300

    Small updates

--- OOP_task/main.py
+++ OOP_task/main.py
@@ -6,15 +6,10 @@ class TrainCounter:
     Count number of wagons in train
     """
     def __init__(self):
-        self.Flag = True
-        self.counter = 0
-        self.position = 0
-        self.state = [True, random.choice([True, False])]
-        """
-        Start
-        """
-        self.loop()
-        self.ans()
+        self._Flag = True
+        self._counter = 0
+        self._position = 0
+        self._state = [True, random.choice([True, False])]
 
     def step(self, length: int = 1, step_mod: bool = True):
         """
@@ -22,38 +17,44 @@ class TrainCounter:
         :param length: Step length
         :param step_mod: If False, we not increase the counter
         """
-        self.position += length
-        self.counter += length * step_mod
+        self._position += length
+        self._counter += length * step_mod
 
     def switch(self):
         """
         Change state in wagon to False. Light off
         """
         train_length = random.randint(1, 10)
-        self.state[self.counter != train_length] = False
+        self._state[self._counter != train_length] = False
 
     def loop(self):
         """
-        Repeating steps, while Flag (Light in 0-wagon) is On
+        Repeating steps, while _Flag (Light in 0-wagon) is On
         """
-        while self.Flag:
+        while self._Flag:
             self.step()
             self.switch()
             self.check()
 
     def check(self):
         """
-        Check Flag state
+        Check _Flag state
         """
-        if self.position:
-            self.step(length=-self.counter, step_mod=False)
+        if self._position:
+            self.step(length=-self._counter, step_mod=False)
             self.check()
         else:
-            self.Flag = self.state[0]
-            self.step(length=self.counter, step_mod=False)
+            self._Flag = self._state[0]
+            self.step(length=self._counter, step_mod=False)
 
-    def ans(self):
+    def ans(self) -> int:
         """
         :return: desired train length
         """
-        return self.counter
+        return self._counter
+
+
+if __name__ == "__main__":
+    count = TrainCounter()
+    count.loop()
+    print(count.ans())
